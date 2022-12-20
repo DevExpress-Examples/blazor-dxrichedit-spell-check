d9789352790301587de4749acf3132965ca74553
SvetlanaMikheeva <80316217+SvetlanaMikheeva@users.noreply.github.com>
2022-12-20 14:54:03 +0400

    Update Program.cs

--- CS/SpellCheck/Program.cs
+++ CS/SpellCheck/Program.cs
@@ -3,6 +3,11 @@ using Microsoft.AspNetCore.Components.Web;
 using DevExpress.Blazor.RichEdit.SpellCheck;
 using Microsoft.Extensions.FileProviders;
 
+var DictionaryFiles = new Dictionary<string, string>()
+{
+    { "de-DE", "de//de.dic" },
+    { "fr-FR", "fr//fr.dic" },
+};
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -22,19 +27,11 @@ builder.Services.AddDevExpressBlazor().AddSpellCheck(opts => {
         Culture = "fr-FR"
     });
     opts.AddToDictionaryAction = (word, culture) => {
-        string path;
-        switch (culture.Name) {
-            case "de-DE":
-                path = opts.FileProvider.GetFileInfo("de//de.dic").PhysicalPath;
-                File.AppendAllText(path, "\n" + word);
-                break;
-            case "fr-FR":
-                path = opts.FileProvider.GetFileInfo("fr//fr.dic").PhysicalPath;
-                File.AppendAllText(path, "\n" + word);
-                break;
-            default:
-                break;
-        };
+        string dictionaryFile = DictionaryFiles.GetValueOrDefault(culture.Name);
+        if (dictionaryFile != default) {
+            var filePath = opts.FileProvider.GetFileInfo(dictionaryFile).PhysicalPath;
+            File.AppendAllText(filePath, "\n" + word);
+        }
     };
     opts.MaxSuggestionCount = 7;
 });
